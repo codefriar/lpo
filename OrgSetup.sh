5e334d54af1af108bbe8b43fbe4cf25998bf1381
Kevin Poorman <kpoorman@salesforce.com>
2018-02-22 15:01:33 -0500

    switching to start branch

--- OrgSetup.sh
+++ OrgSetup.sh
@@ -1,3 +1,4 @@
+git checkout start
 sfdx force:org:create -s -f config/project-scratch-def.json -a LightningPlatformOrientation
 sfdx force:source:push
 sfdx force:user:permset:assign -n Grant_users_access_to_park_management_objects
