75f2be7f0bc4bb2fee57cc4f9f87cfddf6151645
Kevin Poorman <kpoorman@salesforce.com>
2018-02-22 14:45:26 -0500

    starting point

--- OrgSetup.sh
+++ OrgSetup.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -s -f config/project-scratch-def.json -a LightningPlatformOrientation
 sfdx force:source:push
 sfdx force:user:permset:assign -n Grant_users_access_to_park_management_objects
-sfdx force:org:open 
+sfdx force:data:tree:import -p data/Animal__c-plan.json
+sfdx force:data:tree:import -p data/Park__c-plan.json
