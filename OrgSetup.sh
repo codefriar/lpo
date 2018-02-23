b3e6c99fcf368b644b80eaf7fb6d162ad32a9876
Kevin Poorman <kpoorman@salesforce.com>
2018-02-23 10:22:38 -0500

    adding a force:org:open call to the end of the setup script

--- OrgSetup.sh
+++ OrgSetup.sh
@@ -4,3 +4,4 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n Grant_users_access_to_park_management_objects
 sfdx force:data:tree:import -p data/Animal__c-plan.json
 sfdx force:data:tree:import -p data/Park__c-plan.json
+sfdx force:org:open
